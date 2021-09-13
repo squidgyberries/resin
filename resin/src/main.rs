973a3a1ddd5d6cc456a76d83e135c64f03026a45
squidgyberries <43133000+squidgyberries@users.noreply.github.com>
2021-09-13 17:34:02 +0800

    stuff

--- resin/src/main.rs
+++ resin/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    resin_core::thing();
+    let mut args: Vec<String> = std::env::args().collect();
+    let target = args.get_mut(2).unwrap();
+    if !target.ends_with("/") && !target.ends_with("\\") {
+        target.push_str("/");
+    }
+    resin_core::extract(&args[1], &args[2]);
 }
--- resin_core/src/lib.rs
+++ resin_core/src/lib.rs
@@ -1,40 +1,48 @@
 mod resin;
 mod util;
 
-use std::{
-    fs::{create_dir_all, File},
-    io::{BufReader, BufWriter, Write},
-};
+use std::{fs::{create_dir_all, File}, io::{BufReader, BufWriter, Write}, path::Path};
 
 use crate::resin::resin::Resin;
 
-pub fn thing() {
-    let file = File::open("thing.resin").unwrap();
+pub fn extract(in_file: &str, target: &str) {
+    create_dir_all(&target).expect("Unable to create target folder.");
+
+    let file = File::open(in_file).expect("Unable to open resin file.");
     let mut reader = BufReader::new(file);
-    let thing = Resin::new(&mut reader);
-    println!("{}", thing.header.num_empty_dirs);
-    println!("{}", thing.header.num_files);
-    if thing.header.num_empty_dirs > 0 {
-        for i in thing.empty_dirs {
-            println!("{}", i);
-            std::fs::create_dir_all(i).expect("Unable to create directory.");
+    let resin_file = Resin::new(&mut reader);
+    println!("{} empty directories", resin_file.header.num_empty_dirs);
+    println!("{} files", resin_file.header.num_files);
+
+    // Create empty directories
+    if resin_file.header.num_empty_dirs > 0 {
+        for dir in resin_file.empty_dirs {
+            let full_dir_path = format!("{}{}", target, dir);
+            println!("Creating directory {}", dir);
+            create_dir_all(full_dir_path).expect("Unable to create directory.");
         }
     }
-    if thing.header.num_files > 0 {
-        for i in thing.files {
-            println!("{}", i.path);
-            println!("{}", i.data_length);
-            for j in &i.data {
-                print!("{:X} ", j);
-            }
-            println!("");
+
+    // Extract files
+    if resin_file.header.num_files > 0 {
+        for i in resin_file.files {
             let path_split: Vec<&str> = i.path.split("/").collect();
-            let mut folders = String::new();
+
+            // Create directory
+            let mut dirs = String::new();
             for i in 0..path_split.len() - 1 {
-                folders.push_str(&format!("{}/", path_split[i]));
+                dirs.push_str(&format!("{}/", path_split[i]));
             }
-            create_dir_all(folders).expect("Unable to create directory");
-            let file = File::create(i.path).expect("Unable to create file.");
+            let full_dir_path = format!("{}{}", target, dirs);
+            if !Path::new(&full_dir_path).exists() {
+                println!("Creating directory {}", full_dir_path);
+                create_dir_all(full_dir_path).expect("Unable to create directory");
+            }
+
+            // Create file
+            let full_file_path = format!("{}{}", target, i.path);
+            println!("Creating file {}", full_file_path);
+            let file = File::create(full_file_path).expect("Unable to create file.");
             let mut writer = BufWriter::new(file);
             writer.write_all(&i.data).expect("Unable to write to file.");
         }
